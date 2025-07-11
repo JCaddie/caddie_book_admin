df1bbd6542a224a4ab2ec5b3280abc1c84bbcfb7
LeeSeokWoo <indians1720@gmail.com>
2025-07-11 16:53:07 +0900

    refactor(vacation,caddie): 새로운 렌더러 시스템 적용
    • vacation 모듈: 컬럼 함수로 변경
    • caddie 모듈: 컬럼 함수로 변경
    • 기존 함수명 호환성 유지

--- src/modules/caddie/components/index.ts
+++ src/modules/caddie/components/index.ts
@@ -1,3 +1,6 @@
-export * from "./caddie-filter-bar";
+export { default as CaddieFilterBar } from "./caddie-filter-bar";
 export { default as NewCaddieActionBar } from "./new-caddie-action-bar";
-export * from "./new-caddie-columns";
+export { useNewCaddieColumns } from "./new-caddie-columns";
+
+// 이전 이름도 호환성을 위해 유지
+export { useNewCaddieColumns as createNewCaddieColumns } from "./new-caddie-columns";
--- src/modules/vacation/components/index.ts
+++ src/modules/vacation/components/index.ts
@@ -1,2 +1,5 @@
 export { default as VacationActionBar } from "./vacation-action-bar";
-export { default as VacationActionsCell } from "./vacation-actions-cell";
+export { useVacationColumns } from "./vacation-actions-cell";
+
+// 이전 이름도 호환성을 위해 유지
+export { useVacationColumns as VacationActionsCell } from "./vacation-actions-cell";
