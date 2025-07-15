d155addf3664dc5e72a6646bff8dc38fc72eed79
LeeSeokWoo <indians1720@gmail.com>
2025-07-15 16:58:19 +0900

    remove(special-team): 기존 special-team 모듈 제거
    - special-team 모듈을 team 모듈로 완전 이전 완료
    - 중복 코드 제거 및 네이밍 일관성 확보
    - 하위 호환성은 team 모듈의 deprecated alias로 유지

--- src/modules/special-team/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as SpecialTeamCard } from "./special-team-card";
-export { default as SpecialTeamSchedule } from "./special-team-schedule";
-export { default as SpecialTeamStatus } from "./special-team-status";
