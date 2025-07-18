7501f189ffbca315e423a8767a08380a6589aea8
LeeSeokWoo <indians1720@gmail.com>
2025-07-18 19:00:08 +0900

    feat(fields): 필드 일괄 삭제 API 적용
    - 개별 삭제에서 일괄 삭제 API로 변경하여 성능 개선
    - deleteFieldsBulk API 함수 추가 (bulk_delete 엔드포인트 사용)
    - useDeleteFieldsBulk 훅 추가
    - 여러 필드 선택 삭제 시 단일 API 호출로 효율성 향상
    - Promise.all 병렬 호출 방식에서 일괄 처리 방식으로 개선

--- src/modules/field/hooks/index.ts
+++ src/modules/field/hooks/index.ts
@@ -2,5 +2,5 @@ export { useFieldManagement } from "./use-field-management";
 export { useFieldList } from "./use-field-list";
 export { useCreateField } from "./use-create-field";
 export { useUpdateField } from "./use-update-field";
-export { useDeleteField } from "./use-delete-field";
+export { useDeleteField, useDeleteFieldsBulk } from "./use-delete-field";
 export { useFieldDetail } from "./use-field-detail";
